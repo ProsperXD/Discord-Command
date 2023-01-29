4dbb1409d03f9cfec0fa2d2e08126799a940dada
ProsperXD <107730829+ProsperXD@users.noreply.github.com>
2023-01-29 14:29:48 -0800

    Add files via upload

---
+++ discord_cl.lua
@@ -0,0 +1,9 @@
+discord = 'Discord.gg/ExoticDev'
+Title = "Discord Venity!"
+
+RegisterCommand('Discord', function()
+    TriggerEvent('chat:addMessage', {
+    template = '<div style="padding: 0.5vw; margin: 0.5vw; border: 2px solid white; background-color: rgba(145, 57, 53, 0.589); border-radius: 9px;"><i class="fab fa-discord"></i> {0}:<br> '..discord..  ' </div>',
+    args = {Title}
+})
+end)
